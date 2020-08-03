2fd23eaa96ab59e459ac6d067967f7e73aa5abe8
afelix <austinjfelix@gmail.com>
2020-08-03 10:19:53 -0400

    sync

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,27 @@
-import React from 'react'
+import React from 'react';
 
-import Editor from 'mui-editor'
-import 'mui-editor/dist/index.css'
+import { Grid, Card, CardContent, CardActions, Button } from '@material-ui/core';
+
+import Editor from 'mui-editor';
+import 'mui-editor/dist/index.css';
 
 const App = () => {
-  return <Editor />
+    return (
+			<div className='App' >
+				<Grid container justify='center' align='center' >
+					<Grid item xs={10}>
+						<Card>
+							<CardContent>
+								<Editor transparent/>
+							</CardContent>
+							<CardActions>
+								<Button onClick={() => alert('clicked')}>Submit</Button>
+							</CardActions>
+						</Card>
+					</Grid>
+				</Grid>
+			</div>
+    );
 }
 
-export default App
+export default App;
